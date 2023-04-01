467a9ef4c307b9fe905413f10b662c17578fd3ba
TMahto <mahatotanmay99@gmail.com>
2023-04-01 20:48:28 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# My Portfolio
+
+<p><a href="https://kfig21.github.io/portfolio_2022/" target="_blank" rel="noopener noreferrer">View the project here!</a> 👀</p>
+
+<h3>Summary</h3>
+<p>This was the main project for <a href="https://www.theodinproject.com/paths/full-stack-javascript/courses/getting-hired/lessons/building-your-personal-website" target="_blank" rel="noopener noreferrer">The Odin Project's</a> getting hired curriculum. The goal of the project is to build your own portfolio/personal website to host your applications and provide detail about yourself as a developer.</p>
+
+ <h3>Technologies:</h3>
+  <ul>
+  <li>ReactJS</li>
+  <li>SASS</li>
+  <li>Material UI</li>
+  <li>EmailJS</li>
+  <li>StyledComponents</li>
+ </ul>
+
+ <h3>Features:</h3>
+ <ul>
+  <li>Responsive design for desktop, and mobile</li>
+  <li>Different themes made with styled components</li>
+  <li>Animations and pseudo elements.</li>
+  <li>Custom icons and screenshots for each application.</li>
+  <li>EmailJS used for the contact form.</li>
+ </ul>
---
+++ src/components/Projects/HighlightProject.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import ProjectsSC from "./ProjectsSC";
+import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
+import LanguageIcon from "@mui/icons-material/Language";
+import CodeIcon from "@mui/icons-material/Code";
+import SC from "../../themes/SC";
+
+export default function HighlightProject({
+  project,
+  handleDetails,
+  setImageModalProject,
+  setShowImageModal,
+  setImageModalSrc,
+}) {
+  const [mainImgIndex, setMainImgIndex] = useState(0);
+
+  const handleMainImg = (i) => {
+    setImageModalProject(project);
+    setImageModalSrc(i);
+    setShowImageModal(true);
+  };
+
+  // map project technologies
+  const mapTech = (techArr) => {
+    return techArr.map((tech) => (
+      <ProjectsSC.TechWrapper className="tech-wrapper">
+        <ProjectsSC.TechContainer className={`${tech.color}`}>
+          <ProjectsSC.TechIcon>
+            <img src={tech.icon} alt="" />
+          </ProjectsSC.TechIcon>
+          <ProjectsSC.TechName>{tech.color}</ProjectsSC.TechName>
+        </ProjectsSC.TechContainer>
+      </ProjectsSC.TechWrapper>
+    ));
+  };
+  return (
+    <ProjectsSC.HighlightWrapper>
+      {/* LEFT */}
+      <ProjectsSC.HighlightLeft className="left">
+        {/* TITLE */}
+        <ProjectsSC.HighlightTitleContainer className="title-container">
+          <ProjectsSC.FaviconContainer className="favicon-container">
+            <img src={project.favicon} alt="" />
+          </ProjectsSC.FaviconContainer>
+          <ProjectsSC.HighlightTitle
+            className="title"
+            onClick={() => handleDetails(project)}
+          >
+            {project.title.toUpperCase()}
+          </ProjectsSC.HighlightTitle>
+          {/* CLOSE DETAILS */}
+          <CloseFullscreenIcon
+            className="title-icon"
+            onClick={() => handleDetails(project)}
+          />
+          {/* LIVE LINK */}
+          <ProjectsSC.ProjectLink
+            href={project.live}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LanguageIcon className="title-icon" />
+          </ProjectsSC.ProjectLink>
+          {/* REPO */}
+          <ProjectsSC.ProjectLink
+            href={project.repository}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <CodeIcon className="title-icon code" />
+          </ProjectsSC.ProjectLink>
+        </ProjectsSC.HighlightTitleContainer>
+        {/* MOBILE LINKS */}
+        <ProjectsSC.MobileLinks>
+          {/* LIVE LINK */}
+          <ProjectsSC.ProjectLinkMobile
+            href={project.live}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LanguageIcon className="title-icon" />
+          </ProjectsSC.ProjectLinkMobile>
+          {/* REPO */}
+          <ProjectsSC.ProjectLinkMobile
+            href={project.repository}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <CodeIcon className="title-icon code" />
+          </ProjectsSC.ProjectLinkMobile>
+        </ProjectsSC.MobileLinks>
+
+        {/* DESCRIPTION */}
+        <ProjectsSC.HighlightDescContainer className="desc-container">
+          <SC.PrimaryColorText>&gt;</SC.PrimaryColorText> {project.desc}
+        </ProjectsSC.HighlightDescContainer>
+
+        {/* TECH */}
+        <ProjectsSC.HighlightTechContainer>
+          {mapTech(project.icons)}
+        </ProjectsSC.HighlightTechContainer>
+      </ProjectsSC.HighlightLeft>
+      {/* RIGHT */}
+      <ProjectsSC.HighlightRight className="right">
+        <ProjectsSC.HighlightRightInner className="inner">
+          <ProjectsSC.GalleryContainer className="gallery">
+            {project.imgs.map((img, i) => {
+              return (
+                <ProjectsSC.GalleryImg onClick={() => handleMainImg(i)}>
+                  <img
+                    src={img.src}
+                    className={`${mainImgIndex === i && "current"}`}
+                    alt=""
+                  />
+                </ProjectsSC.GalleryImg>
+              );
+            })}
+          </ProjectsSC.GalleryContainer>
+        </ProjectsSC.HighlightRightInner>
+      </ProjectsSC.HighlightRight>
+    </ProjectsSC.HighlightWrapper>
+  );
+}
